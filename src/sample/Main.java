43266a9376c72099ab2937e8d552ac7b57aed863
Steven B Cooley Jr <scooley138@yahoo.com>
2020-01-15 22:21:30 -0800

    Attempting to delete useless files

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,15 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-// This is the original Stage called sample.  I didn't like the name  :)
-// In Hindsight, I still would have named MainScreen something different.
-//    @Override
-//    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
-//    }
 
     @Override
     public void start(Stage mainStage) throws Exception {
