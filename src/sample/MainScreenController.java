15f9711225947ec469aec30674eec0d1020afbe0
Steven B Cooley Jr <scooley138@yahoo.com>
2020-01-16 16:52:55 -0800

    Started adding second scene

--- src/sample/MainScreenController.java
+++ src/sample/MainScreenController.java
@@ -6,12 +6,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
+import java.io.IOException;
+
 public class MainScreenController {
 
     /**
      * Method to change Scene to addPart
      */
-    public void toAddPartScreen(ActionEvent event) {
+    public void toAddPartScreen(ActionEvent event) throws IOException {
         //
         Parent addPart_FXMLLoader = FXMLLoader.load(getClass().getResource("addPart.fxml"));
     }
