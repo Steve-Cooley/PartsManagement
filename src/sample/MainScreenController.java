3a0c69d7c48ae6c96455a0b7408debb290c45d9c
Steven B Cooley Jr <scooley138@yahoo.com>
2020-01-16 18:04:05 -0800

    Started adding second scene

--- src/sample/MainScreenController.java
+++ src/sample/MainScreenController.java
@@ -4,7 +4,11 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.IOException;
 
@@ -12,10 +16,18 @@ public class MainScreenController {
 
     /**
      * Method to change Scene to addPart
+     * fixme note to self, these methods show up in scenebuilder
      */
     public void toAddPartScreen(ActionEvent event) throws IOException {
         //
-        Parent addPart_FXMLLoader = FXMLLoader.load(getClass().getResource("addPart.fxml"));
+        Parent addPartParent = FXMLLoader.load(getClass().getResource("addPart.fxml"));
+        Scene addPartScene = new Scene(addPartParent);
+
+        //Get Stage info
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();  //witchery to me fixme
+
+        window.setScene(addPartScene);
+        window.show();
     }
 
 }
