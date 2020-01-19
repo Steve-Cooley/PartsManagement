d49858ab8e20448e60c5195434c1bcc231cd7349
Steven B Cooley Jr <scooley138@yahoo.com>
2020-01-18 21:11:58 -0800

    Started each class.

---
+++ src/sample/Inventory.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.collections.ObservableList;
+
+public class Inventory {
+    private ObservableList<Part> allParts;
+    private ObservableList<Product> allProducts;
+
+    public void addPart() {
+
+    }
+
+    public void addProduct() {}
+
+    //public Part lookupPart() {} //fixme
+}
---
+++ src/sample/Outsourced.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Outsourced extends Part {
+
+    private String companyName;
+
+    public Outsourced(int id, String name, double price, int stock, int min, int max) {
+        super(id, name, price, stock, min, max);
+    }
+
+    public Outsourced(int id, String name, double price, int stock, int min, int max, String companyName) {
+        super(id, name, price, stock, min, max);
+        this.companyName = companyName;
+    }
+}
