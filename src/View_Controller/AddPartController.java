9c336eea467cb90392ba407d3a7d739edb021af4
Steven B Cooley Jr <scooley138@yahoo.com>
2020-01-25 17:25:15 -0800

    table views on main page exit button main page new Product constructor

--- src/View_Controller/AddPartController.java
+++ src/View_Controller/AddPartController.java
@@ -8,10 +8,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,6 +17,7 @@ import java.util.ResourceBundle;
 
 public class AddPartController implements Initializable {
 
+    @FXML private Button saveButton;
     @FXML private TextField idField;
     @FXML private TextField nameField;
     @FXML private TextField invField;
