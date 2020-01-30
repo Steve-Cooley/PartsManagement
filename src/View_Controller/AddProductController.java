5e5dd4b195e35817e33d8ca1f6fdd2f251c24dc8
Steven B Cooley Jr <scooley138@yahoo.com>
2020-01-29 18:09:27 -0800

    Giving up on search for now, trying to work on product pages.

--- src/View_Controller/AddProductController.java
+++ src/View_Controller/AddProductController.java
@@ -1,7 +1,9 @@
 package View_Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,31 +17,32 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class AddProductController {
-    public Button saveButton;
-    public Button cancelButton;
-    public TextField idField;
-    public TextField nameField;
-    public TextField invField;
-    public TextField priceField;
-    public TextField maxField;
-    public Label minLabel;
-    public TextField minField;
-    public Button searchButton;
-    public TextField searchBar;
-    public TableColumn topIDTableCol;
-    public TableColumn topPartNTableCol;
-    public TableColumn topInvTableCOl;
-    public TableColumn topPriceTableCol;
-    public TableColumn botPartIDTableCol;
-    public TableColumn botNameTableCol;
-    public TableColumn botInvTableCol;
-    public TableColumn botPriceCol;
+public class AddProductController implements Initializable {
+
+    @FXML private Button saveButton;
+    @FXML private Button cancelButton;
+    @FXML private TextField idField;
+    @FXML private TextField nameField;
+    @FXML private TextField invField;
+    @FXML private TextField priceField;
+    @FXML private TextField maxField;
+    @FXML private Label minLabel;
+    @FXML private TextField minField;
+    @FXML private Button searchButton;
+    @FXML private TextField searchBar;
+    @FXML private TableColumn topIDTableCol;
+    @FXML private TableColumn topPartNTableCol;
+    @FXML private TableColumn topInvTableCOl;
+    @FXML private TableColumn topPriceTableCol;
+    @FXML private TableColumn botPartIDTableCol;
+    @FXML private TableColumn botNameTableCol;
+    @FXML private TableColumn botInvTableCol;
+    @FXML private TableColumn botPriceCol;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         //
-
+        System.out.println("Initialize is running!");
     }
 
     public void toMainScene(ActionEvent event) throws IOException {
